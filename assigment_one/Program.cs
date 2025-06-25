5b1392a4254cda35eee8618165d46c173d662378
Ina-Apdidahir <150722233+Ina-Apdidahir@users.noreply.github.com>
2025-06-25 23:30:21 +0300

    Add project files.

---
+++ assigment_one/Program.cs
@@ -0,0 +1,31 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+
+string name = "Abdirahman Abdikarim isse";
+Console.WriteLine(name);
+
+string mother_name = "Hawo Farah";
+Console.WriteLine(mother_name);
+
+string date_of_birth = "14/2/2002";
+string numeri_date = date_of_birth.Replace("/", "");
+int date_as_int = Convert.ToInt32(numeri_date);
+
+Console.WriteLine("string date : "+ date_of_birth);
+Console.WriteLine("intiger date : "+ date_as_int);
+
+string place_of_birth = "galkaio";
+Console.WriteLine(place_of_birth);
+
+string address = "Unisom street Galkaio";
+Console.WriteLine(address);
+
+int phone = 907702811;
+Console.WriteLine(phone);
+
+string sex = "Man";
+Console.WriteLine(sex);
+
+string status = "single";
+Console.WriteLine(status);
